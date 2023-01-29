1986d6585849329e464f53f47fcbbc14b7ae1409
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-01-29 00:41:17 -0500

    added colors to the views

--- Cookcademy/Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Cookcademy/Views/RecipesListView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct RecipesListView: View {
     @StateObject var recipeData = RecipeData()
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         List {
             ForEach(recipeData.recipes) { recipe in
                 NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle("All Recipes")
     }
